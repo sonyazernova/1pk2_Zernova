ee80f2c6da0732496bd1dd3b24a881ef6e9ddfa8
sonyazernova <sonyazernova@bk.ru>
2025-01-21 22:39:34 +0500

    выполнена 4 задача 1 урока

---
+++ Lesson_01/Task_01_04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите свой год рождения: ");
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите свой месяц рождения: ");
+int month = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите свой день рождения: ");
+int day = int.Parse(Console.ReadLine());
+
+DateTime today = DateTime.Today;
+DateTime birthDate = new DateTime(year, month, day);
+int age = today.Year - birthDate.Year;
+if (birthDate > today.AddYears(-age)) age--;
+
+if (age >= 18)
+    Console.WriteLine("Пользователь совершеннолетний ");
+else
+    Console.WriteLine("Пользователь не совершеннолетний");
